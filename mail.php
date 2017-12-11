6a340772b95f5dc6c74c1e143ebe1dbac9891cc4
valentyn.melnyk <l7l23qp@gmail.com>
2017-12-11 21:55:46 +0200

    asiadevelopment 11.12.2017

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 
-$recepient = "youmail@ya.ru";
-$sitename = "Название сайта";
+$recepient = "l7l23qp@gmail.com";
+$sitename = "Asiadevelopment";
 
-$name = trim($_GET["name"]);
-$phone = trim($_GET["phone"]);
-$text = trim($_GET["text"]);
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nE-mail: $email \nТекст: $text";
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$pagetitle = "Новая сообщение с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
